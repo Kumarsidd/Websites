8b3ad53fc4089a6000cce57d1460feb3ef584739
Kumarsidd <93262021+Kumarsidd@users.noreply.github.com>
2022-04-01 20:32:24 +0530

    Delete Another directory

--- Another/app.js
+++
@@ -1,9 +0,0 @@
-const menu = document.getElementById("menu");
-const navbar = document.querySelector(".navbar");
-
-const navLink = () =>{
-	menu.addEventListener('click', ()=> {
-		navbar.classList.toggle("nav-toggle");
-	});
-}
-navLink();
